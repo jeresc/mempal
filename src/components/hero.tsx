44d820737d62adf1fde28420f35dc4c7f583758c
jeresc <jeremiastomassrc@gmail.com>
2024-03-15 22:57:12 -0300

    feat: 💄 enhance Landing styles

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,12 +2,12 @@ import {WaitlistForm} from "@/components/waitlist";
 
 export function Hero() {
   return (
-    <section className="grid grid-cols-1 place-items-stretch gap-4 gap-y-12 md:grid-cols-2">
-      <section className="flex h-full max-w-xl flex-col items-center justify-center gap-3 text-center md:items-start md:gap-4 md:text-left">
-        <h1 className="rn-clr-content-primary my-0 text-4xl font-bold leading-[106%] tracking-tight text-black xl:text-6xl xl:text-[48px]">
+    <section className="grid grid-cols-1 place-items-center gap-4 gap-y-12 md:grid-cols-2">
+      <section className="flex h-full max-w-xl flex-col items-center justify-center gap-3 py-8 text-center md:items-start md:gap-4 md:py-0 md:text-left">
+        <h1 className="my-0 max-w-[320px] text-pretty text-[42px] font-bold leading-[106%] tracking-tight text-slate-900 md:max-w-none md:text-4xl xl:text-[42px]">
           Unlock your true potential
         </h1>
-        <sub className="my-0 mb-0 text-lg leading-6 text-gray-400 md:pr-8 xl:text-2xl">
+        <sub className="my-0 mb-0 text-balance text-lg leading-6 text-gray-400 md:pr-8 xl:text-xl">
           Supercharge your learning with our intelligent study platform. Generate personalized
           questions and flashcards directly from your resources.
         </sub>
